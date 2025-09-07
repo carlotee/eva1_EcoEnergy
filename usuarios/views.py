492221e2b63f6036b2cf86aa549385e8213bda7f
carlotee <carlosvega3031@gmail.com>
2025-09-07 02:16:04 -0300

    se actualiza views en usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import RegistroEmpresaForm
 # Create your views here.
 def login(request):
     return render(request, 'usuarios/login.html')
 
-def registro(request):
-    return render(request, 'usuarios/registro.html')
-
 def contraseña(request):
     return render(request, 'usuarios/contraseña.html')
+
+def registro_empresa(request):
+    if request.method == 'POST':
+        form = RegistroEmpresaForm(request.POST)
+        if form.is_valid():
+            # Crear el usuario
+            User.objects.create_user(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+            return render(request, 'usuarios/registro_exito.html')  # Página de confirmación
+    else:
+        form = RegistroEmpresaForm()
+    
+    return render(request, 'usuarios/registro_empresa.html', {'form': form})
